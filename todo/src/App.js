d017e1470d770d4c903504652cbd194c241f6491
chrislane134 <christopherlane101@gmail.com>
2020-05-26 09:24:58 +0100

    added header

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
 import './App.css';
 
 
@@ -46,6 +47,7 @@ class App extends Component {
     
   return (
     <div className="App">
+      <Header />
       <Todos todos={this.state.todos} markComplete={this.markComplete} 
       delTodo={this.delTodo}/>
       
