77a939ade10a60a4801db7d3e3981dbe99b94913
chrislane134 <christopherlane101@gmail.com>
2020-05-26 09:16:39 +0100

    added delete button and working

--- todo/src/App.js
+++ todo/src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     ]
   }
 
+  // Toggle Complete
   markComplete = (id) => {
     this.setState({ todos: this.state.todos.map(todo => {
       if(todo.id === id) {
@@ -36,11 +37,17 @@ class App extends Component {
     }) });
   }
 
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
   render() {
     
   return (
     <div className="App">
-      <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      <Todos todos={this.state.todos} markComplete={this.markComplete} 
+      delTodo={this.delTodo}/>
       
     </div>
   );
